e17c6ba6129d86d7c01e5f8d3caf5c60dfbe1216
Sheldon Perkins <csperkins18@gmail.com>
2020-02-08 04:13:55 -0500

    generate reandom code

--- public/js/game.js
+++ public/js/game.js
@@ -1,53 +1,59 @@
-$('#btn-start').on('click', function(event){
+$('#btn-start').on('click', function (event) {
     $("#start-game").hide();
     $("#in-game").show();
     $(".main-container").show();
 });
 
-$("#btn-restart").on('click', function(event){
-// new game function
+$("#btn-restart").on('click', function (event) {
+    // new game function
 });
 
-$("#btn-rule").on('click', function(event){
+$("#btn-rule").on('click', function (event) {
     $("#rules-text").toggle();
 });
-$("#btn-rules").on('click', function(event){
+$("#btn-rules").on('click', function (event) {
     $("#rules-text").toggle();
 });
 
-// 2 Arrays for generate code to be broken and user guesses
-var code = [];
-var guess = [];
+$(function () {
+    // 2 Arrays for generate code to be broken and user guesses
+    var code = [];
+    var guess = [];
 
-// hints
-var incrementHint = 1;
+    // hints
+    var incrementHint = 1;
 
-// increment rows
-var incrementRound = 1;
+    // increment rows
+    var incrementRound = 1;
 
-// user slots
-var slot = {
-    1: "red",
-    2: "blue",
-    3: "green",
-    4: "orange",
-    5: "pink",
-    6: "yellow"
-};
+    // user slots
+    var slot = {
+        1: "red",
+        2: "blue",
+        3: "green",
+        4: "orange",
+        5: "pink",
+        6: "yellow"
+    };
 
-// ___________FUNCTIONS_____________
+    // ___________FUNCTIONS_____________
 
-// game setup
+    // game setup
 
-//  generate random code to be broken
+    //  generate random code to be broken
+    $(function generateCode() {
+        for (i = 0; i < 4; i++) {
+            code[i] = Math.floor(Math.random() * 7);
+        }
+    });
+    // insert user guess
 
-// insert user guess
+    // compare user guess to code
 
-// compare user guess to code
+    // display hints
 
-// display hints
+    // new game reset board generate new code
 
-// new game reset board generate new code
+    // setup end game 
 
-// setup end game 
-
+});
