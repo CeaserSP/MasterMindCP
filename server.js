61b79cb6631da0313f041d51904f55c68e31384f
Sheldon Perkins <csperkins18@gmail.com>
2020-02-08 01:25:37 -0500

    Import routes and give the server access to them.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 express = require('express');
+path = require("path");
 
 var PORT = process.env.PORT || 3307;
 
@@ -15,10 +16,13 @@ var exhb = require("express-handlebars");
 
 app.engine("handlebars", exhb({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.set('views', path.join(__dirname, 'views'));
 
 // _________________ROUTES______________________
 // Import routes and give the server access to them.
+var routes = require("./controllers/game-controller.js");
 
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
